a176c5fb30a44aeebd7d3c913a64c1b4909f1da0
Madhawa mirihagalla <slonline1232@GMAIL.COM>
2026-05-05 21:10:01 +0530

    Mr_white (bug fix 2)

--- src/main/java/sliit/oop_server_app/config/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package sliit.oop_server_app.config.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import sliit.oop_server_app.entity.Category;
-
-@Repository
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-}
